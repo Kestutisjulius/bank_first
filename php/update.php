6c7e97f4a3a938024eea8d16f9d4d2fbb1d717d5
Kestutis <kestutis.julius@gmail.com>
2022-05-30 17:23:05 +0300

    work with update

--- php/update.php
+++ php/update.php
@@ -3,32 +3,48 @@ if (isset($_GET['update'])){
     $current_data = file_get_contents(__DIR__ . '/../data/users.json');
     $arr_data = json_decode($current_data, true);
     $index = $_GET['update'];
-    $currentUser = $arr_data[$index];
+    $currentUser = [];
+    foreach ($arr_data as $key => $value){
+        if ($key == $index){
+            $currentUser = $value;
+        }
+    }
 
 ?>
-<div class="workingSpace">
-    <div class="inf">
-        <h1 class="h1">EDIT user form :</h1>
-        <form action="./php/write.php" method="post" class="form"> <!-- action - i kur siusti -->
-            <label for="<?php echo $currentUser['Avatar name']; ?>">Avatar Name: </label>
-            <input type="text" id="aname" name="<?php echo $currentUser['Avatar name']; ?>">
-            <label for="<?php echo $currentUser['Name']; ?>">Name: </label>
-            <input type="text" id="name" name="<?php echo $currentUser['Name']; ?>">
-            <label for="<?php echo $currentUser['Surname']; ?>">Surname: </label>
-            <input type="text" id="sname" name="<?php echo $currentUser['Surname']; ?>">
-
-            <input type="radio" id="male" name="<?php echo ($currentUser['Gender'] == 'male') ? print_r('checked="checked"') : print_r('')  ?>" value="male">
-            <label for="male">male</label>
-
-            <input type="radio" id="female" name="<?php echo ($currentUser['Gender'] == 'female') ? print_r('checked="checked"') : print_r('')  ?>" value="female">
-            <label for="female" >female</label><div class="br"></div>
-
-            <label>Contribution €: <?php echo $currentUser['Sum input']; ?></label>
-
-            <textarea id="user_info" name="user_info" rows="4" cols="58"><?php echo $currentUser['Additional info']; ?></textarea>
-            <div class="submit"><input id="submit" type="submit" value="Submit" class="submit_a" name="create"></div>
-        </form>
-    </div>
-</div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../CSS/forEdit.css">
+        <title>K-Bank Edit</title>
+    </head>
+        <div class="background font">
+            <h1 class="h1-update">Update: <?php echo $currentUser['Name']?> with ID [<?php echo $currentUser['id']?>]</h1>
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post" class="form"> <!-- action - i kur siusti -->
+                <div class="gender"><?php echo $currentUser['Gender']?></div>
+                <label class="label" for="aname">Avatar Name: </label>
+                <input type="text" id="aname" name="avatar_name" value="<?php echo $currentUser['Avatar name']?>">
+                <label class="label" for="name">Name: </label>
+                <input type="text" id="name" name="name" value="<?php echo $currentUser['Name']?>">
+                <label class="label" for="sname">Surname: </label>
+                <input type="text" id="sname" name="sname" value="<?php echo $currentUser['Surname']?>">
+                <textarea class="txtArea" id="user_info" name="user_info" rows="4" cols="58"><?php echo $currentUser['Additional info']?></textarea>
+                <div class="br"></div>
+                <div class="haveFund">Has: <?php echo $currentUser['Sum input']?>&#8364.</div>
+                <div class="flex">
+                    <div class="together">
+                        <label for="sum_in" class="sumLabel">add funds:</label>
+                        <input type="number" id="sum_in" name="sum_in" placeholder="+ ...Euro" class="sumInput">
+                    </div>
+                    <div class="together">
+                        <label for="sum_out" class="sumLabel">lost funds:</label>
+                        <input type="number" id="sum_out" name="sum_out" placeholder="minus ...Euro" class="sumInput">
+                    </div>
+                </div>
+                    <div class="submit"><input id="submit" type="submit" value="Submit" class="submit_a" name="edit"></div>
+            </form>
+        </div>
 <?php
 }
+
--- php/write.php
+++ php/write.php
@@ -1,19 +1,21 @@
 <?php
 if (isset($_POST['create'])) {
     date_default_timezone_set("Europe/Vilnius");
+    $current_data = file_get_contents(__DIR__ . '/../data/users.json');
+    $arr_data = json_decode($current_data, true);
+    $sessionSum = floatval($_POST['sum_in']);
+    $cash = $arr_data['Sum_input'];
     $recordLine = [
         'id' => uniqid(),
         'Avatar name' => $_POST['avatar_name'],
         'Name' => $_POST['name'],
         'Surname' => $_POST['sname'],
         'Gender' => (isset($_POST['gender'])) ? $_POST['gender'] : 'unknown',
-        'Sum input' => $_POST['sum_in'],
-        'Additional info' => $_POST['user_info'],
-        'Data&Time' => Date("Y-m-d H:i:s")
+        'Sum_input' => $cash + $sessionSum,
+        'Additional_info' => $_POST['user_info'],
+        'Data&Time' => [Date("Y-m-d H:i:s"), floatval($_POST['sum_in'])]
     ];
 
-    $current_data = file_get_contents(__DIR__ . '/../data/users.json');
-    $arr_data = json_decode($current_data, true);
     $arr_data[] = $recordLine;
     $updated_data = json_encode($arr_data);
 
@@ -22,6 +24,6 @@ if (isset($_POST['create'])) {
     }
     file_put_contents(__DIR__ . '/../data/users.json', $updated_data);
 
-    header('Location: http://localhost/vienaragiai/bank_first/?u=1');
+    header('Location: http://localhost/bank_first/?u=1');
     die;
 }
